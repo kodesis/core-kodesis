3661896ee9fc7264948c3b7a5f78d19821343403
Akbar Anung <akbaranung06@gmail.com>
2024-11-20 16:41:30 +0700

    fix bug comment activity dan ubah warna tema

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -321,8 +321,7 @@ class Task extends CI_Controller
 	function activity_comment()
 	{
 		// Looping all files
-		if (isset($_FILES['file'])) {
-
+		if ($_FILES['file']) {
 			$files = $_FILES;
 			$cpt = count($_FILES['file']['name']);
 
@@ -346,58 +345,59 @@ class Task extends CI_Controller
 				} else {
 					$arr_att[] = $att_name;
 					$arr_name[] = $name;
-				}
-			}
-			// var_dump(array($arr_att));
-			// var_dump($this->upload->data()['file_size']);
-			if ($this->upload->data()['file_size'] < 10000) {
-				$id_detail = $this->input->post('id_detail');
-				$get_task_detail = $this->db->query("SELECT * FROM task as a left join task_detail as b on(a.id=b.id_task) where b.id_detail='$id_detail'")->row_array();
-				$phone_x = explode(';', $get_task_detail['member']);
-				// print_r($phone_x);
-				// exit;
-				foreach ($phone_x as $k) { //member card kirim ke wa
-					$get_user = $this->db->get_where('users', ['nip' => $k])->row_array();
-					if ($get_user) {
-						$task_name = $get_task_detail['task_name'];
-						$nama_member = $get_user["nama"];
-						$comment = $this->input->post("commentt");
-						$nama_session = $this->session->userdata('nama');
-						$msg = "There's a new comment\nCard Name : *$task_name*\nComment : *$comment*\n\nComment from :  *$nama_session*";
-						$this->api_whatsapp->wa_notif($msg, $get_user['phone']);
+					if ($this->upload->data()['file_size'] < 10000) {
+						$id_detail = $this->input->post('id_detail');
+						$get_task_detail = $this->db->query("SELECT * FROM task as a left join task_detail as b on(a.id=b.id_task) where b.id_detail='$id_detail'")->row_array();
+						$phone_x = explode(';', $get_task_detail['member']);
+						// print_r($phone_x);
+						// exit;
+						foreach ($phone_x as $k) { //member card kirim ke wa
+							$get_user = $this->db->get_where('users', ['nip' => $k])->row_array();
+							if ($get_user) {
+								$task_name = $get_task_detail['task_name'];
+								$nama_member = $get_user["nama"];
+								$comment = $this->input->post("commentt");
+								$nama_session = $this->session->userdata('nama');
+								$msg = "There's a new comment\nCard Name : *$task_name*\nComment : *$comment*\n\nComment from :  *$nama_session*";
+								$this->api_whatsapp->wa_notif($msg, $get_user['phone']);
+							}
+						}
+					} else {
+						echo "<script>alert('File Tidak boleh lebih dari 10Mb !');window.history.back();</>";
 					}
 				}
-
-				$data = [
-					"id_task_detail" => $this->input->post('id_detail'),
-					"comment_member" => $this->input->post('commentt'),
-					"attachment" => implode(';', $arr_att),
-					"attachment_name" => implode(';', str_replace(' ', '_', $arr_name)),
-					"member" => $this->session->userdata('nip')
-
-				];
-
-				$this->db->insert('task_detail_comment', $data);
-
-				// update task detail
-				$this->db->set('read', '0');
-				$this->db->where('id_detail', $id_detail);
-				$this->db->update('task_detail');
-
-				//Update Task
-				$task_detail = $this->db->get_where('task_detail', ['id_detail' => $id_detail])->row();
-				$task = $this->db->get_where('task', ['id' => $task_detail->id_task])->row();
-
-				$this->db->set('read', '0');
-				$this->db->where('id', $task->id);
-				$this->db->update('task');
-				redirect('task/task_view/' . $this->input->post('id_task') . '/' . $this->input->post('id_detail'));
-			} else {
-				echo "<script>alert('File Tidak boleh lebih dari 10Mb !');window.history.back();</script>";
 			}
 		} else {
-			redirect('task/task_view/' . $this->input->post('id_task') . '/' . $this->input->post('id_detail'));
+			$arr_att[] = null;
+			$arr_name[] = null;
 		}
+		// var_dump(array($arr_att));
+		// var_dump($this->upload->data()['file_size']);
+
+		$data = [
+			"id_task_detail" => $this->input->post('id_detail'),
+			"comment_member" => $this->input->post('commentt'),
+			"attachment" => implode(';', $arr_att),
+			"attachment_name" => implode(';', str_replace(' ', '_', $arr_name)),
+			"member" => $this->session->userdata('nip')
+
+		];
+
+		$this->db->insert('task_detail_comment', $data);
+
+		// update task detail
+		$this->db->set('read', '0');
+		$this->db->where('id_detail', $id_detail);
+		$this->db->update('task_detail');
+
+		//Update Task
+		$task_detail = $this->db->get_where('task_detail', ['id_detail' => $id_detail])->row();
+		$task = $this->db->get_where('task', ['id' => $task_detail->id_task])->row();
+
+		$this->db->set('read', '0');
+		$this->db->where('id', $task->id);
+		$this->db->update('task');
+		redirect('task/task_view/' . $this->input->post('id_task') . '/' . $this->input->post('id_detail'));
 	}
 	public function set_upload_options($file_path)
 	{

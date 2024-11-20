553b4a99c70c54f3888503988c0164925874d0cf
Akbar Anung <akbaranung06@gmail.com>
2024-11-20 12:07:40 +0700

    fix bug create card task

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -819,7 +819,7 @@ class Task extends CI_Controller
 					$nama_filee[] = str_replace(' ', '', time() . '_' . $_FILES['att']['name'][$xx]);
 				}
 			} else {
-				$nama_filee = null;
+				$nama_filee[] = null;
 			}
 
 			if ($this->input->post('project_name') != '') {
